1751f5484a78f619553be2fee03a2f11bf08be89
BadHenry7 <141955809+BadHenry7@users.noreply.github.com>
2025-07-26 12:11:59 -0500

    Cmabios en navbar admin, para cerrar sesion y validacion user

--- src/app/Componentes/navbar-administrador/navbar-administrador.component.ts
+++ src/app/Componentes/navbar-administrador/navbar-administrador.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar-administrador',
@@ -11,4 +11,26 @@ import { RouterLink } from '@angular/router';
 export class NavbarAdministradorComponent {
  henry : boolean= true
   image : String=''
+constructor (private router:Router){}
+
+   confirmacion() {
+        Swal.fire({
+      title: "¿Estas seguro?",
+      text: "Se perdera lo que no hayas guardado",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, cerrar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.clear();
+          this.router.navigate(['/']);
+
+       // window.location.href = "/";
+      }
+    });
+    
+}
+ 
 }
--- src/app/Page/login/login.component.ts
+++ src/app/Page/login/login.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';//para usar ngModel
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';//Para usar router navigate
-import {  UsersService } from '../../services/usuarios.service';
-import {User} from '../../interfaces/usuarios'
-
+import { UsersService } from '../../services/usuarios.service';
+import { User } from '../../interfaces/usuarios'
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   imports: [RouterLink, FormsModule],
@@ -67,20 +67,41 @@ export class LoginComponent implements OnInit {
 
           this.verificar = true;
           console.log("*//////////////", todo[0].rol)
-          let encontrado = { name: todo[0].nombre, correo: this.correo, password: this.password, id: todo[0].id};
+          let encontrado = { name: todo[0].nombre, correo: this.correo, password: this.password, id: todo[0].id };
           console.log("Imprimos el encontrado", encontrado);
           let miStorage = window.localStorage;
           miStorage.setItem("usuario", JSON.stringify(encontrado));
 
           if (todo[0].rol == 1) {
+            Swal.fire({
+              //Popup window position, can be 'top', 'top-start', 'top-end', 'center', 'center-start', 'center-end', 'bottom', 'bottom-start', or 'bottom-end'.
+              position: "top",
+              icon: "success",
+              title: "Inicio de sesion exitoso, bienvenido " + todo[0].nombre,
+              showConfirmButton: false,
+            });
 
             this.router.navigate(['administrador/principal']);
 
           } else if (todo[0].rol == 2) {
             this.router.navigate(['pacientes/principal']);
+            Swal.fire({
+              //Popup window position, can be 'top', 'top-start', 'top-end', 'center', 'center-start', 'center-end', 'bottom', 'bottom-start', or 'bottom-end'.
+              position: "top",
+              icon: "success",
+              title: "Inicio de sesion exitoso, bienvenido " + todo[0].nombre,
+              showConfirmButton: false,
+            });
 
           } else {
             this.router.navigate(['doctor/principal']);
+            Swal.fire({
+              //Popup window position, can be 'top', 'top-start', 'top-end', 'center', 'center-start', 'center-end', 'bottom', 'bottom-start', or 'bottom-end'.
+              position: "top",
+              icon: "success",
+              title: "Inicio de sesion exitoso, bienvenido " + todo[0].nombre,
+              showConfirmButton: false,
+            });
           }
 
 
@@ -94,7 +115,12 @@ export class LoginComponent implements OnInit {
 
       }, error: (error) => {
         if (error.status === 404) {
-          alert("Usuario o contraseña incorrectos.");
+         Swal.fire({
+                    title: "Error!",
+                    text: "La combinación entre usuario y contraseña es incorrecta",
+                    icon: "error",
+                    confirmButtonText: "Cool",
+                });
         } else {
           console.log("Error del servidor: " + error.status);
         }
