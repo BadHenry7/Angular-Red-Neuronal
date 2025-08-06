db8f29767ab4ede84e2b3e5c7443d96d77263bf5
BadHenry7 <141955809+BadHenry7@users.noreply.github.com>
2025-08-06 14:19:23 -0500

    Confirmacion para cerrar sesion en rol doctor y paciente

--- src/app/Componentes/navbar-doctor/navbar-doctor.component.ts
+++ src/app/Componentes/navbar-doctor/navbar-doctor.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar-doctor',
@@ -11,6 +12,8 @@ import { RouterLink } from "@angular/router";
 
 export class NavbarDoctorComponent implements OnInit {
 
+  constructor(private router: Router){}
+
   nombre: string = "Doctor";
 
   ngOnInit(): void {
@@ -20,5 +23,27 @@ export class NavbarDoctorComponent implements OnInit {
       const usuario = JSON.parse(usuarioGuardado);
       this.nombre = usuario.name;
     }
+  }
+
+
+  
+     confirmacion() {
+          Swal.fire({
+        title: "¿Estas seguro?",
+        text: "Se perdera lo que no hayas guardado",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, cerrar!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          localStorage.clear();
+            this.router.navigate(['/']);
+  
+         // window.location.href = "/";
+        }
+      });
+      
   }
 }
--- src/app/Componentes/navbar-paciente/navbar-paciente.component.ts
+++ src/app/Componentes/navbar-paciente/navbar-paciente.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-navbar-paciente',
@@ -11,15 +13,38 @@ import { RouterLink } from '@angular/router';
 
 export class NavbarPacienteComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
   nombre: string = "Paciente";
 
   ngOnInit(): void {
-      const usuarioGuardado = localStorage.getItem('usuario');
+    const usuarioGuardado = localStorage.getItem('usuario');
 
     if (usuarioGuardado) {
       const usuario = JSON.parse(usuarioGuardado);
       this.nombre = usuario.name;
-    } 
+    }
+  }
+
+
+  confirmacion() {
+    Swal.fire({
+      title: "¿Estas seguro?",
+      text: "Se perdera lo que no hayas guardado",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, cerrar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.clear();
+        this.router.navigate(['/']);
+
+        // window.location.href = "/";
+      }
+    });
+
   }
 
 }
